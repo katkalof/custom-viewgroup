3efbfa90cb4b032404f86e9ebe97289fdd13dae7
katkalof <katkalof@gmail.com>
2016-08-09 21:11:49 +0300

    add onlayout visibility==gone check

--- app/src/main/java/ru/yandex/yamblz/ui/layouts/CustomLayout.java
+++ app/src/main/java/ru/yandex/yamblz/ui/layouts/CustomLayout.java
@@ -85,6 +85,9 @@ public class CustomLayout extends ViewGroup {
         int childCount = getChildCount();
         for (int i = 0; i < childCount; ++i) {
             View child = getChildAt(i);
+            if (child.getVisibility() == GONE) {
+                continue;
+            }
             int childWidth = child.getMeasuredWidth();
             int childHeight = child.getMeasuredHeight();
             child.layout(horizontalOffset, verticalOffset, horizontalOffset + childWidth, verticalOffset + childHeight);
